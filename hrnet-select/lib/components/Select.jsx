aef03c329c7c8723a089c93bc38ad28c6a87fbef
ElodieAllegro <124352648+ElodieAllegro@users.noreply.github.com>
2025-02-12 17:29:00 +0100

    Select css

--- hrnet-select/lib/components/Select.jsx
+++ hrnet-select/lib/components/Select.jsx
@@ -1,9 +1,39 @@
-const Select =() => {
+import { useState } from 'react'
+import Caret from './caret.svg'
+import PropTypes from 'prop-types';
+import './select.css'
+
+const Select =({options, onChange, defaultValue, placeholder}) => {
+    const [isListOpen, setIsListOpen] = useState(false);
+
+    const handleSelect = (e) => {
+        e.preventDefault()
+        onChange(e.target.value);
+        setIsListOpen(false);// ferme la liste après la sélection
+
+    }
     return (
-        <div>
-     
+        <div className="custom-select" onClick={() => setIsListOpen(!isListOpen)}>
+            <div className="select-selected">
+                {defaultValue || placeholder}
+                <img src={Caret} alt="caret" className="select-caret" />
+        </div>
+        {isListOpen && (
+     <ul className="select-items">
+       {options.map((s) => <li key={s.id}>
+         <a href="" onClick={(e) => handleSelect(e, s.value)}>{s.value}</a>
+       </li>)}                
+     </ul>
+   )}
         </div>
     );
 }
+//prototypes
 
+Select.propTypes = {
+    options: PropTypes.array,
+    onChange: PropTypes.func,
+    defaultValue: PropTypes.string,
+    placeholder: PropTypes.string,
+ }
 export default Select;
